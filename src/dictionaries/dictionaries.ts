5f2b928a461c25845c9a7b2c60fadbcdef155e29
Mila <milaarroyo65@gmail.com>
2025-08-13 20:48:17 -0600

    refactor: add explicit Locale type for stronger TS checks

--- src/dictionaries/dictionaries.ts
+++ src/dictionaries/dictionaries.ts
@@ -1,9 +1,12 @@
-import 'server-only'
- 
+ "use server";
+
 const dictionaries = {
-  en: () => import('./en.json').then((module) => module.default),
-  es: () => import('./es.json').then((module) => module.default),
-}
- 
-export const getDictionary = async (locale: 'en' | 'es') =>
-  dictionaries[locale]()
+	en: () => import("./en.json").then((module) => module.default),
+	es: () => import("./es.json").then((module) => module.default),
+} as const;
+
+type Locale = keyof typeof dictionaries;
+
+export const getDictionary = async (locale: Locale) => {
+	return dictionaries[locale]();
+};
