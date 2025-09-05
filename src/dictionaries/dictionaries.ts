ff897d617c47443d21f7f0dc3903fe3c91297df5
Mila <milaarroyo65@gmail.com>
2025-09-04 20:06:16 -0600

    refactor dictionary loading to use async/await for improved readability

--- src/dictionaries/dictionaries.ts
+++ src/dictionaries/dictionaries.ts
@@ -1,12 +1,19 @@
- "use server";
+"use server";
+import { Dictionary } from "@/lib/types";
 
 const dictionaries = {
-	en: () => import("./en.json").then((module) => module.default),
-	es: () => import("./es.json").then((module) => module.default),
+	en: async (): Promise<Dictionary> => {
+		const module = await import("./en.json");
+		return module.default;
+	},
+	es: async (): Promise<Dictionary> => {
+		const module = await import("./es.json");
+		return module.default;
+	},
 } as const;
 
 type Locale = keyof typeof dictionaries;
 
 export const getDictionary = async (locale: Locale) => {
-	return dictionaries[locale]();
+	return await dictionaries[locale]();
 };
