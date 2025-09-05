9262190a20d55b2eafcb6c02bab7dc7b8bed4afd
Mila <milaarroyo65@gmail.com>
2025-09-04 21:25:15 -0600

    refactor: move description to dictionaries and inject it as html into the component

--- src/components/more.tsx
+++ src/components/more.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useLocale } from "@/contexts/locale-context";
 import Image from "next/image";
 
 export default function MoreAbout() {
+	const { dict } = useLocale();
 	return (
 		<section
 			id="more-about"
@@ -19,34 +22,10 @@ export default function MoreAbout() {
 				</h1>
 			</div>
 			<div className="flex flex-col md:flex-row items-center justify-center w-9/10 gap-8 mb-6">
-				<div className="prose prose-base text-muted-foreground text-justify text-lg">
-					<p>
-						Hi! I'm Mila, a 25-year-old self-taught web developer based in
-						Mexico City. I’m bilingual (Spanish is my first language, and I’ve
-						studied English since childhood). I also enjoy learning other
-						languages, I can even read and write some Korean.
-						<br />
-						<br />I originally studied graphic design and earned an associate’s
-						degree in high school, with plans to pursue that career. But during
-						college, I took a <em>Web Design</em> class where I discovered how
-						much I enjoyed working with HTML and CSS. That experience completely
-						shifted my path: I realized I was more passionate about building
-						websites than designing them.
-						<br />
-						<br />
-						Since then, I’ve been focused on learning full-stack web
-						development. I’ve completed online courses like{" "}
-						<em>The Complete Full-Stack Web Development Bootcamp</em> and{" "}
-						<em>100 Days of Code: The Complete Python Pro Bootcamp</em> by Dr.
-						Angela Yu, and I continue to build projects to strengthen my skills.
-						<br />
-						<br />
-						Now, I’m working toward a career in web development, combining my
-						design background with my passion for coding to create websites that
-						are both functional and visually engaging.
-					</p>
-					
-				</div>
+				<div
+					className="prose prose-base text-muted-foreground text-justify text-lg"
+					dangerouslySetInnerHTML={{ __html: dict["more-section"] }}
+				></div>
 			</div>
 		</section>
 	);
